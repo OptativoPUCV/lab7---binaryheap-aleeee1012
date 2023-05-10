35c68d88694c81b69e20b7499ba7177d549fa3f3
Replit user <>
2023-05-10 23:24:36 +0000

    new try

--- heap.c
+++ heap.c
@@ -5,23 +5,24 @@
 #include <ctype.h>
 #include "heap.h"
 
-typedef struct nodo{
-   void* data;
-   int priority;
+typedef struct nodo
+{
+  void* data;
+  int priority;
 }heapElem;
 
-typedef struct Heap{
+typedef struct Heap
+{
   heapElem* heapArray;
   int size;
   int capac;
 } Heap;
 
 
-void* heap_top(Heap* pq){
-    return NULL;
+void* heap_top(Heap* pq)
+{
+  return NULL;
 }
-
-
 
 void heap_push(Heap* pq, void* data, int priority){
 
@@ -32,7 +33,12 @@ void heap_pop(Heap* pq){
 
 }
 
-Heap* createHeap(){
+Heap* createHeap()
+{
+  Heap *arreglo = (Heap*)malloc(Heap);
 
-   return NULL;
+  arreglo->heapArray = (int*)malloc(3 * sizeof(int));
+  arreglo->size = 0;
+  arreglo->capac = 3;
+  return arreglo;
 }
